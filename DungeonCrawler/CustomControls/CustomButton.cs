5b4c9f8c98012abd42e4e67bf4afdb973bdd3035
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-18 21:24:38 +0200

    Added custom button

---
+++ DungeonCrawler/CustomControls/CustomButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DungeonCrawler.CustomControls
+{
+    class CustomButton : Button
+    {
+        public Color ButtonColor { get; set; }
+        public Color BorderColor { get; set; }
+
+        public CustomButton() : base()
+        {
+            // Set style optimized for user drawn controls
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer
+                | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.ResizeRedraw, true);
+
+            // Set FlatStyle that looks better when drawn over
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderSize = 0;
+
+            // Set default Colors. Inferit BorderColor from parent
+            ButtonColor = Color.Transparent;
+            BorderColor = BackColor;
+            BackColor = Color.Transparent;
+
+            Paint += new PaintEventHandler(CustomButton_Paint);
+        }
+
+        private void CustomButton_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
