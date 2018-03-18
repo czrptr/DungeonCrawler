ef183d80dbd2caaa029c3dcde68f7a7b452a8921
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-19 01:23:58 +0200

    Made it imutable

--- DungeonCrawler/Graphics/CustomBorderDimensions.cs
+++ DungeonCrawler/Graphics/CustomBorderDimensions.cs
@@ -4,30 +4,49 @@
     {
         public CustomBorderDimensions(int cutSize)
         {
-            H1Size = H2Size = H3Size = V1Size = V2Size = V3Size = cutSize;
+            _h1Size = _h2Size = _h3Size = _v1Size = _v2Size = _v3Size = cutSize;
         }
 
         public CustomBorderDimensions(int cornerSize, int edgeSize)
         {
-            H1Size = H3Size = V1Size = V3Size = cornerSize;
-            H2Size = V2Size = edgeSize;
+            _h1Size = _h3Size = _v1Size = _v3Size = cornerSize;
+            _h2Size = _v2Size = edgeSize;
         }
 
         public CustomBorderDimensions(int h1Size, int h2Size, int h3Size, int v1Size, int v2Size, int v3Size)
         {
-            H1Size = h1Size;
-            H2Size = h2Size;
-            H3Size = h3Size;
-            V1Size = v1Size;
-            V2Size = v2Size;
-            V3Size = v3Size;
+            _h1Size = h1Size;
+            _h2Size = h2Size;
+            _h3Size = h3Size;
+            _v1Size = v1Size;
+            _v2Size = v2Size;
+            _v3Size = v3Size;
         }
 
-        public int H1Size { get; set; }
-        public int H2Size { get; set; }
-        public int H3Size { get; set; }
-        public int V1Size { get; set; }
-        public int V2Size { get; set; }
-        public int V3Size { get; set; }
+        private int _h1Size, _h2Size, _h3Size, _v1Size, _v2Size, _v3Size;
+
+        public int HSize(int count)
+        {
+            if (count == 1)
+                return _h1Size;
+            else if (count == 2)
+                return _h2Size;
+            else if (count == 3)
+                return _h3Size;
+            else
+                return -1;
+        }
+
+        public int VSize(int count)
+        {
+            if (count == 1)
+                return _v1Size;
+            else if (count == 2)
+                return _v2Size;
+            else if (count == 3)
+                return _v3Size;
+            else
+                return -1;
+        }
     }
 }
