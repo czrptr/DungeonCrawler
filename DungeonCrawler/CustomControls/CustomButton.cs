d63690825189d83efe80016c328db0e89c86b107
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-19 16:55:09 +0200

    Need to solve Desing Time property setting

--- DungeonCrawler/CustomControls/CustomButton.cs
+++ DungeonCrawler/CustomControls/CustomButton.cs
@@ -5,13 +5,49 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows.Forms;
+using DungeonCrawler.Graphix;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace DungeonCrawler.CustomControls
 {
     class CustomButton : Button
     {
+        private Image _borderImage = Properties.Resources.Button_Menus;
+        private CustomBorderDimensions _borderDimensions = new CustomBorderDimensions(3, 1);
+        private CustomBorder _border;
+
+        [Category("Custom")]
+        [Description("The color of the button.")]
         public Color ButtonColor { get; set; }
+
+        [Category("Custom")]
+        [Description("The color of the button border.")]
         public Color BorderColor { get; set; }
+
+        [Category("Custom")]
+        [Description("The Image used to generate the border.")]
+        public Image BorderImage
+        {
+            get => _borderImage;
+            set
+            {
+                _borderImage = value;
+                _border = CustomBorder.FromImage(_borderImage, _borderDimensions);
+            }
+        }
+
+        [Category("Custom")]
+        [Description("The dimensions of the border.")]
+        public CustomBorderDimensions BorderDimensions
+        {
+            //get => _borderDimensions;
+            set
+            {
+                _borderDimensions = value;
+                _border = CustomBorder.FromImage(_borderImage, _borderDimensions);
+            }
+        }
 
         public CustomButton() : base()
         {
@@ -19,7 +55,6 @@ namespace DungeonCrawler.CustomControls
             SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer
                 | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer
                 | ControlStyles.ResizeRedraw, true);
-
             // Set FlatStyle that looks better when drawn over
             FlatStyle = FlatStyle.Flat;
             FlatAppearance.BorderSize = 0;
@@ -29,13 +64,15 @@ namespace DungeonCrawler.CustomControls
             BorderColor = BackColor;
             BackColor = Color.Transparent;
 
-            // Set custom draw
-            Paint += new PaintEventHandler(CustomButton_Paint);
+            // Set default look to Menu Button
+            _border = CustomBorder.FromImage(_borderImage, _borderDimensions);
         }
 
-        private void CustomButton_Paint(object sender, PaintEventArgs e)
+        // Drawing custom border
+        protected override void OnPaint(PaintEventArgs e)
         {
-            
+            base.OnPaint(e);
+            e.Graphics.FillCustomBorder(_border, DisplayRectangle);
         }
     }
 }
