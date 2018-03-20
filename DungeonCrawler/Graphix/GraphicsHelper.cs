f20f74eed45c33d52ea2622ad1ec197b626bdaf3
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-20 21:49:35 +0200

    Implemented better BorderDraw

--- DungeonCrawler/Graphix/GraphicsHelper.cs
+++ DungeonCrawler/Graphix/GraphicsHelper.cs
@@ -16,44 +16,65 @@ namespace DungeonCrawler.Graphix
             graphics.FillRectangle(brush, x, y, 1, 1);
         }
 
-        public static void FillCustomBorder(this Graphics graphics, CustomBorder customBorder, Rectangle rect)
+        public static void FillCustomBorder(this Graphics graphics, Image borderImage, Rectangle drawRect, Rectangle cutRect)
         {
-            // Drawing each corner (from top-left counterclockwise)
-            graphics.DrawImage(customBorder[0, 0], rect.Left, rect.Top);
-            graphics.DrawImage(customBorder[0, 1], rect.Left + rect.Width - customBorder.VSize(3), rect.Top);
-            graphics.DrawImage(customBorder[0, 2], rect.Left + rect.Width - customBorder.VSize(3), rect.Top + rect.Height - customBorder.HSize(3));
-            graphics.DrawImage(customBorder[0, 3], rect.Left, rect.Top + rect.Height - customBorder.HSize(3));
+            //graphics.FillRectangle(Brushes.LightGreen, drawRect);
+            int leftX = borderImage.Width - cutRect.X - cutRect.Width;
+            int bottomY = borderImage.Height - cutRect.Y - cutRect.Height;
 
             // Initialize WrapMode for drawing the edges and fill
             ImageAttributes imgAtt = new ImageAttributes();
             imgAtt.SetWrapMode(WrapMode.Tile);
 
-            /* Drawing each border (from top counterclockwise) */
+            // Prepare new Rectangle for drawing slices
+            Rectangle sliceRect;
 
-            // Create a rectagle reprezenting the portion in witch the border will be drawn
-            Rectangle edgeRect = new Rectangle(rect.Left + customBorder.VSize(1), rect.Top, rect.Width - customBorder.VSize(1) - customBorder.VSize(3), customBorder.HSize(1));
-            // Fill that rectangle with the edge sprite
-            graphics.DrawImage(customBorder[1, 0], edgeRect, 0, 0, edgeRect.Width, edgeRect.Height, GraphicsUnit.Pixel, imgAtt);
+            // Drawing each corner (from top-left counterclockwise)
+            sliceRect = new Rectangle(drawRect.X, drawRect.Y, cutRect.X, cutRect.Y);
+            graphics.DrawImage(borderImage, sliceRect, 0, 0, sliceRect.Width, sliceRect.Height, GraphicsUnit.Pixel, imgAtt);
 
-            // Repeat for each edge
-            edgeRect.Y += rect.Height - customBorder.HSize(3);
-            graphics.DrawImage(customBorder[1, 2], edgeRect, 0, 0, edgeRect.Width, edgeRect.Height, GraphicsUnit.Pixel, imgAtt);
+            sliceRect = new Rectangle(drawRect.X + drawRect.Width - leftX, drawRect.Y, leftX, cutRect.Y);
+            graphics.DrawImage(borderImage, sliceRect, cutRect.X + cutRect.Width, 0, sliceRect.Width, sliceRect.Height, GraphicsUnit.Pixel, imgAtt);
 
-            edgeRect = new Rectangle(rect.Left, rect.Top + customBorder.HSize(1), customBorder.VSize(1), rect.Height - customBorder.HSize(1) - customBorder.HSize(3));
-            graphics.DrawImage(customBorder[1, 3], edgeRect, 0, 0, edgeRect.Width, edgeRect.Height, GraphicsUnit.Pixel, imgAtt);
+            sliceRect = new Rectangle(drawRect.X, drawRect.Y + drawRect.Height - bottomY, cutRect.X, bottomY);
+            graphics.DrawImage(borderImage, sliceRect, 0, cutRect.Y + cutRect.Height, sliceRect.Width, sliceRect.Height, GraphicsUnit.Pixel, imgAtt);
 
-            edgeRect.X += rect.Width - customBorder.VSize(3);
-            graphics.DrawImage(customBorder[1, 1], edgeRect, 0, 0, edgeRect.Width, edgeRect.Height, GraphicsUnit.Pixel, imgAtt);
+            sliceRect = new Rectangle(drawRect.X + drawRect.Width - leftX, drawRect.Y + drawRect.Height - bottomY, leftX, bottomY);
+            graphics.DrawImage(borderImage, sliceRect, cutRect.X + cutRect.Width, cutRect.Y + cutRect.Height, sliceRect.Width, sliceRect.Width, GraphicsUnit.Pixel, imgAtt);
 
-            // Fill middle with CustomBorder.Fill
-            edgeRect = new Rectangle(rect.Left + customBorder.VSize(1), rect.Top + customBorder.HSize(1),
-                rect.Width - customBorder.VSize(1) - customBorder.VSize(3), rect.Height - customBorder.HSize(1) - customBorder.HSize(3));
-            graphics.DrawImage(customBorder.Body, edgeRect, 0, 0, edgeRect.Width, edgeRect.Height, GraphicsUnit.Pixel, imgAtt);
-        }
+            // Prepare new Bitmap for storing edge tiles
+            // For some reason even if the sliceRect is perfet ti still doesn't draw correctly
+            // It draws sliceRect.Width + 1
+            // graphics.DrawImage(borderImage, sliceRect, cutRect.X, 0, cutRect.Width, cutRect.Y, GraphicsUnit.Pixel, imgAtt);
+            Bitmap edgeSprite;
 
-        public static void FillCustomBorder(this Graphics graphics, CustomBorder customBorder, int x, int y, int width, int height)
-        {
-            FillCustomBorder(graphics, customBorder, new Rectangle(x, y, width, height));
+            // Drawing each border (from top counterclockwise)
+            sliceRect = new Rectangle(drawRect.X + cutRect.X, drawRect.Y, drawRect.Width - cutRect.X - leftX, cutRect.Y);
+            edgeSprite = new Bitmap(cutRect.Width, cutRect.Y);
+            Graphics.FromImage(edgeSprite).DrawImage(borderImage, -cutRect.X, 0);
+            graphics.DrawImage(edgeSprite, sliceRect, 0, 0, edgeSprite.Width, edgeSprite.Height, GraphicsUnit.Pixel, imgAtt);
+
+            sliceRect.Y += drawRect.Height - bottomY;
+            sliceRect.Height = bottomY;
+            edgeSprite = new Bitmap(cutRect.Width, bottomY);
+            Graphics.FromImage(edgeSprite).DrawImage(borderImage, -cutRect.X, -cutRect.Y - cutRect.Height);
+            graphics.DrawImage(edgeSprite, sliceRect, 0, 0, edgeSprite.Width, edgeSprite.Height, GraphicsUnit.Pixel, imgAtt);
+
+            sliceRect = new Rectangle(drawRect.X, drawRect.Y + cutRect.Y, cutRect.X, drawRect.Height - cutRect.Y - bottomY);
+            edgeSprite = new Bitmap(cutRect.X, cutRect.Height);
+            Graphics.FromImage(edgeSprite).DrawImage(borderImage, 0, -cutRect.Y);
+            graphics.DrawImage(edgeSprite, sliceRect, 0, 0, edgeSprite.Width, edgeSprite.Height, GraphicsUnit.Pixel, imgAtt);
+
+            sliceRect.X += drawRect.Width - leftX;
+            sliceRect.Width = leftX;
+            edgeSprite = new Bitmap(leftX, cutRect.Height);
+            Graphics.FromImage(edgeSprite).DrawImage(borderImage, -cutRect.X - cutRect.Width, -cutRect.Y);
+            graphics.DrawImage(edgeSprite, sliceRect, 0, 0, edgeSprite.Width, edgeSprite.Height, GraphicsUnit.Pixel, imgAtt);
+
+            sliceRect = new Rectangle(drawRect.X + cutRect.X, drawRect.Y + cutRect.Y, drawRect.Width - cutRect.X - leftX, drawRect.Height - cutRect.Y - bottomY);
+            edgeSprite = new Bitmap(cutRect.Width, cutRect.Height);
+            Graphics.FromImage(edgeSprite).DrawImage(borderImage, -cutRect.X, -cutRect.Y);
+            graphics.DrawImage(edgeSprite, sliceRect, 0, 0, edgeSprite.Width, edgeSprite.Height, GraphicsUnit.Pixel, imgAtt);
         }
     }
 }
