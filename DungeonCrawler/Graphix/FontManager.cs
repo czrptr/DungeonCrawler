b1022fdcb74d3f99f9b4c68d7c6d48474df4f82d
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-23 00:26:38 +0200

    Added class for importing fonts

---
+++ DungeonCrawler/Graphix/FontManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Text;
+
+namespace DungeonCrawler.Graphix
+{
+    static class FontManager
+    {
+        private static PrivateFontCollection collection = new PrivateFontCollection();
+
+        static FontManager()
+        {
+            string fontFolder = Directory.GetCurrentDirectory();
+            fontFolder = Directory.GetParent(fontFolder).FullName;
+            fontFolder = Directory.GetParent(fontFolder).FullName;
+            fontFolder += @"Resources\Fonts\";
+            collection.AddFontFile("VCR_OSD_MONO.ttf");
+        }
+    }
+}
