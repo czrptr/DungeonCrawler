ae1d7f7e5abafa1cfaff8bbc4be4affc0b88f930
Cezar Petriuc <cezar.c.petriuc@gmail.com>
2018-03-26 12:38:16 +0300

    Ready for actions

--- DungeonCrawler/MainWindow.Designer.cs
+++ DungeonCrawler/MainWindow.Designer.cs
@@ -28,63 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.dcMenuButtonControl1 = new DungeonCrawler.CustomControls.DCMenuButtonControl();
-            this.dcMenuButtonControl2 = new DungeonCrawler.CustomControls.DCMenuButtonControl();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(114, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // dcMenuButtonControl1
-            // 
-            this.dcMenuButtonControl1.BackColor = System.Drawing.Color.Transparent;
-            this.dcMenuButtonControl1.Location = new System.Drawing.Point(253, 118);
-            this.dcMenuButtonControl1.Name = "dcMenuButtonControl1";
-            this.dcMenuButtonControl1.Size = new System.Drawing.Size(109, 70);
-            this.dcMenuButtonControl1.TabIndex = 5;
-            this.dcMenuButtonControl1.Text = "dcMenuButtonControl1";
-            // 
-            // dcMenuButtonControl2
-            // 
-            this.dcMenuButtonControl2.BackColor = System.Drawing.Color.Transparent;
-            this.dcMenuButtonControl2.Location = new System.Drawing.Point(339, 151);
-            this.dcMenuButtonControl2.Name = "dcMenuButtonControl2";
-            this.dcMenuButtonControl2.Size = new System.Drawing.Size(121, 60);
-            this.dcMenuButtonControl2.TabIndex = 6;
-            this.dcMenuButtonControl2.Text = "dcMenuButtonControl2";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(558, 367);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dcMenuButtonControl2);
-            this.Controls.Add(this.dcMenuButtonControl1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ClientSize = new System.Drawing.Size(556, 400);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.Load += new System.EventHandler(this.MainWindow_Load);
-            this.Controls.SetChildIndex(this.dcMenuButtonControl1, 0);
-            this.Controls.SetChildIndex(this.dcMenuButtonControl2, 0);
-            this.Controls.SetChildIndex(this.button1, 0);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private CustomControls.DCMenuButtonControl dcMenuButtonControl1;
-        private CustomControls.DCMenuButtonControl dcMenuButtonControl2;
     }
 }
--- DungeonCrawler/MainWindow.cs
+++ DungeonCrawler/MainWindow.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Diagnostics;
-using DungeonCrawler.Graphix;
-using DungeonCrawler.CustomControls;
-using System.IO;
-using System.Drawing.Text;
+﻿// -----------------------------------------------------------------------
+// <copyright file="MainWindow.cs">
+//     Copyright (c) Cezar Petriuc. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace DungeonCrawler
 {
-    public partial class MainWindow : DCForm
-    {
+    using DungeonCrawler.CustomControls;
 
+    /// <inheritdoc />
+    public partial class MainWindow : GameForm
+    {
+        #region Constructors
+
+        /// <inheritdoc />
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MainWindow_Load(object sender, EventArgs e)
+        #endregion
+
+        private void MainWindow_Load(object sender, System.EventArgs e)
         {
-            
+
         }
     }
 }
